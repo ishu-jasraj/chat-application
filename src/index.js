b8d5ed39c01fd8bb2b72b04c9b35c042caaaa59a
Ishu Kumari Jasrajpuria <ishu.jasraj@gmail.com>
2023-05-11 18:41:15 +0530

    count event created

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
 
-const port = process.env.PORT || 3000;
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-const app = express();
-const server = http.createServer(app);
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-const publicPath = path.join(__dirname,'../public');
-app.use(express.static(publicPath));
+app.use(express.static(publicDirectoryPath))
+let clients = 0;
+let count = 0;
+io.on('connection', (socket) => {
+    clients++;
+    console.log('New WebSocket connection ',clients);
+    socket.emit('countUpdated',count);
+    socket.on('increment',()=>{
+        count++;
+        // socket.emit('countUpdated',count);
+        io.emit('countUpdated',count);
+    })
+})
 
-app.get('/',(req,res)=>{
-});
-
-server.listen(port,()=>{
-    console.log("server up and running on port ",port); 
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
 })
